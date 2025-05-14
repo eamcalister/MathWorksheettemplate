d0c647071e94d4c48480c7e847249db911dba672
eamcalister <mcalister_e@fortlewis.edu>
2025-05-14 08:47:39 -0600

    building syllabus, fix chapter headings

--- output/web/lunr-pretext-search-index.js
+++ output/web/lunr-pretext-search-index.js
@@ -14,9 +14,9 @@ var ptx_lunr_docs = [
   "level": "1",
   "url": "ch-syllabus.html",
   "type": "Chapter",
-  "number": "1",
+  "number": "",
   "title": "Course Syllabus",
-  "body": " Course Syllabus   Welcome Math 121 at Fort Lewis College.    Course Information      Course Title : Precalculus     Course Number : MATH 121     Credits : 4    Section 1 Time and Room(s) : 8:00-8:55am, MWF Noble 110, Th Noble 160   Section 2 Time and Room(s) : 10:10-11:05am, MWF Noble 110, Th Berndt 340   Section 3 Time and Room(s) : 11:25am-12:20pm, MWF Noble 110, Th Berndt 340   Section 4 Time and Room(s) : 2:30-3:25pm, MWF Noble 110, Th Berndt 340   Instructors:  Sections 1 and 3 - Erich McAlister (mcalister_e@fortlewis.edu), Sections 2 and 4 - Levi Lorenzo (email)      Course Description  This course covers the topics in algebra and trigonometry necessary for students who plan to enroll in MATH 221. Topics include functions, compositions and inverses, and domain and range. Polynomial, rational, exponential, logarithmic, and trigonometric functions are examined in depth, with an emphasis on symbolic methods, parameters that describe each family of functions, and multiple representations of functions (algebraic, numerical, graphical, and verbal representations).    Grading  This course has no grades because it is just a demo.   "
+  "body": " Course Syllabus   Welcome Math 121 at Fort Lewis College.    Course Information      Course Title : Precalculus     Course Number : MATH 121     Credits : 4    Section 1 Time and Room(s): 8:00-8:55am, MWF Noble 110, Th Noble 160   Section 2 Time and Room(s): 10:10-11:05am, MWF Noble 110, Th Berndt 340   Section 3 Time and Room(s): 11:25am-12:20pm, MWF Noble 110, Th Berndt 340   Section 4 Time and Room(s): 2:30-3:25pm, MWF Noble 110, Th Berndt 340   Instructors:  Sections 1 and 3 - Erich McAlister (mcalister_e@fortlewis.edu), Sections 2 and 4 - Levi Lorenzo (email)      Course Description  This course covers the topics in algebra and trigonometry necessary for students who plan to enroll in MATH 221. Topics include functions, compositions and inverses, and domain and range. Polynomial, rational, exponential, logarithmic, and trigonometric functions are examined in depth, with an emphasis on symbolic methods, parameters that describe each family of functions, and multiple representations of functions (algebraic, numerical, graphical, and verbal representations).    Instructional Format  This is a face-to-face course, which means we will meet in person.    Course Materials  Required Text: Functions Modeling Change: A Preparation for Calculus - 6th Edition by Connally et. al. The text    Student Office Hours     Erich McAlister's Office Hours:      Levi Lorenzo's Office Hours:       Feedback and Communication  Communication of all course information will occur via Canvas as well as in class. Most assignments will be submitted and graded online in Canvas or Webwork.   "
 },
 {
   "id": "course-info-2",
@@ -25,14 +25,14 @@ var ptx_lunr_docs = [
   "type": "Paragraph (with a defined term)",
   "number": "",
   "title": "",
-  "body": "Course Title Course Number Credits Section 1 Time and Room(s) Section 2 Time and Room(s) Section 3 Time and Room(s) Section 4 Time and Room(s) Instructors: "
+  "body": "Course Title Course Number Credits Section 1 Time and Room(s): Section 2 Time and Room(s): Section 3 Time and Room(s): Section 4 Time and Room(s): Instructors: "
 },
 {
   "id": "notes-week-01",
   "level": "1",
   "url": "notes-week-01.html",
   "type": "Section",
-  "number": "2.1",
+  "number": "",
   "title": "Week 1",
   "body": " Week 1  This week we covered the following topics:  "
 },
@@ -41,7 +41,7 @@ var ptx_lunr_docs = [
   "level": "1",
   "url": "notes-week-02.html",
   "type": "Section",
-  "number": "2.2",
+  "number": "",
   "title": "Week 2",
   "body": " Week 2  This week we covered the following topics in the second week.  "
 },
@@ -50,7 +50,7 @@ var ptx_lunr_docs = [
   "level": "1",
   "url": "Activity-magic-beans.html",
   "type": "Worksheet",
-  "number": "3.1",
+  "number": "",
   "title": "Magic Beans",
   "body": " Magic Beans   You have traded your cow for 5 magic chocolate covered espresso beans. Each night at midnight, each bean splits into three beans. To take advantage of this, you eat 8 beans each morning for breakfast. You wonder how many beans you will have after breakfast 30 days after you traded your cow.  Let be the sequence of number of beans you have days after trading your cow, (after breakfast, before midnight; take ).      Write out the first few terms of the sequence. Then give a recursive definition for the sequence and explain how you know it is correct.      What do you notice about all elements of the sequence? Will they all be ...? Prove your conjecture by mathematical induction.        Find a closed formula for and prove it is correct using induction.     "
 },
@@ -59,7 +59,7 @@ var ptx_lunr_docs = [
   "level": "2",
   "url": "Activity-magic-beans.html#Activity-magic-beans-3-1",
   "type": "Worksheet Exercise",
-  "number": "3.1.1",
+  "number": "1",
   "title": "",
   "body": "  Write out the first few terms of the sequence. Then give a recursive definition for the sequence and explain how you know it is correct.   "
 },
@@ -68,7 +68,7 @@ var ptx_lunr_docs = [
   "level": "2",
   "url": "Activity-magic-beans.html#Activity-magic-beans-3-2",
   "type": "Worksheet Exercise",
-  "number": "3.1.2",
+  "number": "2",
   "title": "",
   "body": "  What do you notice about all elements of the sequence? Will they all be ...? Prove your conjecture by mathematical induction.   "
 },
@@ -77,7 +77,7 @@ var ptx_lunr_docs = [
   "level": "2",
   "url": "Activity-magic-beans.html#Activity-magic-beans-4-1",
   "type": "Worksheet Exercise",
-  "number": "3.1.3",
+  "number": "3",
   "title": "",
   "body": "  Find a closed formula for and prove it is correct using induction.   "
 },
