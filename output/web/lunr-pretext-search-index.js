504cc56ff337157ab3d2bde93f5c27409b3a13a3
eamcalister <mcalister_e@fortlewis.edu>
2025-05-16 16:47:05 -0600

    added template worksheet

--- output/web/lunr-pretext-search-index.js
+++ output/web/lunr-pretext-search-index.js
@@ -15,8 +15,8 @@ var ptx_lunr_docs = [
   "url": "Activity-title.html",
   "type": "Worksheet",
   "number": "",
-  "title": "Week 2 - Section 6.1 Continued",
-  "body": " Week 2 - Section 6.1 Continued  Linear and Quadratic Functions as Shifts and Stretches  The families of all linear and quadratic functions can each be built using simpler functions: (for positive values of ) and (for positive values of ), for linear and quadratic functions, respectively. What does this mean exactly? It means every linear function is a shift and\/or reflection of and every quadratic function is a shift and\/or reflection of . In this worksheet we'll explore this idea.      Consider the problem: find a linear function with slope whose -intercept is at .    Find the formula for and sketch a graph of it.                  The function you found is a vertical shift of a simpler function. Which simpler function is it? Explain in your own words and show this simpler function in your graph.      Now suppose that instead of having a -intercept at , we want a function that passes through the point . What additional transformation needs to be applied to your basic function from part (b)? Find the formula for this transformation and graph it. How else have we described this formula for a linear function?          The graph below shows the graph of and a another parabola that is the result of some sequence of transformations of .    Describe the transformations applied to , in order, to obtain . (There are three of them and the order they're performed matters.)       f(x) = x^2  g(x) = -(x-5)^2+3    \\               Find the formula for based on your answer to (a). What form of a quadratic function is this?      Suppose you take and reflect its graph across the -axis, then reflect it across the -axis resulting in a new function .  i  Sketch the graph of on the given axes.   ii  How can you obtain the graph of as transformations of (without going to first)?             Another way to find the vertex of a quadratic using simpler functions and shifts. Consider the function   We can write this as a vertical shift of the function upward by units. Notice that   Notice that (from which came) has easily identified zeros at and . It's vertex lies halfway between them at . Then we can compute the -value of this vertex to be Thus, the vertex of is units higher at . So the vertex form of is  Your task: Use the idea from the example to put the function into vertex form.     "
+  "title": "Activity Title",
+  "body": " Activity Title   This is the introduction to the activity.      This is the first exercise.     "
 },
 {
   "id": "Activity-title-3-1",
@@ -25,21 +25,39 @@ var ptx_lunr_docs = [
   "type": "Worksheet Exercise",
   "number": "1",
   "title": "",
+  "body": "  This is the first exercise.   "
+},
+{
+  "id": "Week2-Sec-6_1",
+  "level": "1",
+  "url": "Week2-Sec-6_1.html",
+  "type": "Worksheet",
+  "number": "",
+  "title": "Week 2 - Section 6.1 Continued",
+  "body": " Week 2 - Section 6.1 Continued  Linear and Quadratic Functions as Shifts and Stretches  The families of all linear and quadratic functions can each be built using simpler functions: (for positive values of ) and (for positive values of ), for linear and quadratic functions, respectively. What does this mean exactly? It means every linear function is a shift and\/or reflection of and every quadratic function is a shift and\/or reflection of . In this worksheet we'll explore this idea.      Consider the problem: find a linear function with slope whose -intercept is at .    Find the formula for and sketch a graph of it.                  The function you found is a vertical shift of a simpler function. Which simpler function is it? Explain in your own words and show this simpler function in your graph.      Now suppose that instead of having a -intercept at , we want a function that passes through the point . What additional transformation needs to be applied to your basic function from part (b)? Find the formula for this transformation and graph it. How else have we described this formula for a linear function?          The graph below shows the graph of and a another parabola that is the result of some sequence of transformations of .    Describe the transformations applied to , in order, to obtain . (There are three of them and the order they're performed matters.)       f(x) = x^2  g(x) = -(x-5)^2+3    \\               Find the formula for based on your answer to (a). What form of a quadratic function is this?      Suppose you take and reflect its graph across the -axis, then reflect it across the -axis resulting in a new function .  i  Sketch the graph of on the given axes.   ii  How can you obtain the graph of as transformations of (without going to first)?             Another way to find the vertex of a quadratic using simpler functions and shifts. Consider the function   We can write this as a vertical shift of the function upward by units. Notice that   Notice that (from which came) has easily identified zeros at and . It's vertex lies halfway between them at . Then we can compute the -value of this vertex to be Thus, the vertex of is units higher at . So the vertex form of is  Your task: Use the idea from the example to put the function into vertex form.     "
+},
+{
+  "id": "Week2-Sec-6_1-3-1",
+  "level": "2",
+  "url": "Week2-Sec-6_1.html#Week2-Sec-6_1-3-1",
+  "type": "Worksheet Exercise",
+  "number": "1",
+  "title": "",
   "body": "  Consider the problem: find a linear function with slope whose -intercept is at .    Find the formula for and sketch a graph of it.                  The function you found is a vertical shift of a simpler function. Which simpler function is it? Explain in your own words and show this simpler function in your graph.      Now suppose that instead of having a -intercept at , we want a function that passes through the point . What additional transformation needs to be applied to your basic function from part (b)? Find the formula for this transformation and graph it. How else have we described this formula for a linear function?     "
 },
 {
-  "id": "Activity-title-4-1",
+  "id": "Week2-Sec-6_1-4-1",
   "level": "2",
-  "url": "Activity-title.html#Activity-title-4-1",
+  "url": "Week2-Sec-6_1.html#Week2-Sec-6_1-4-1",
   "type": "Worksheet Exercise",
   "number": "2",
   "title": "",
   "body": "  The graph below shows the graph of and a another parabola that is the result of some sequence of transformations of .    Describe the transformations applied to , in order, to obtain . (There are three of them and the order they're performed matters.)       f(x) = x^2  g(x) = -(x-5)^2+3    \\               Find the formula for based on your answer to (a). What form of a quadratic function is this?      Suppose you take and reflect its graph across the -axis, then reflect it across the -axis resulting in a new function .  i  Sketch the graph of on the given axes.   ii  How can you obtain the graph of as transformations of (without going to first)?        "
 },
 {
-  "id": "Activity-title-5-1",
+  "id": "Week2-Sec-6_1-5-1",
   "level": "2",
-  "url": "Activity-title.html#Activity-title-5-1",
+  "url": "Week2-Sec-6_1.html#Week2-Sec-6_1-5-1",
   "type": "Worksheet Exercise",
   "number": "3",
   "title": "",
